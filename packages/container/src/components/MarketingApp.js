d5e36b3dd9250d356a1063903f848427f1ca2a03
Francisco Rubial <franjorub@gmail.com>
2023-03-01 23:31:59 -0400

    feat: creating initial setup

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,12 @@
+import React, { useRef, useEffect } from "react";
+import { mount } from "marketing/MarketingApp";
+
+export default function MarketingApp() {
+  const ref = useRef();
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+}
